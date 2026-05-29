2c74c7e34ad0c93cc069027667467fbe23734b5d
Jery Alexander Barrientos Peraza <jbarrientosp@miumg.edu.gt>
2026-05-29 12:40:19 -0600

    El usuario ingresa manualmente el contenido de la variable

--- src/Inicio/Main.java
+++ src/Inicio/Main.java
@@ -1,12 +1,20 @@
 package Inicio;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
+		
+		Scanner sc =new Scanner(System.in);
+		
 System.out.println("===== BIBLIOTECA BASE =====");
+System.out.println("Ingrese el titulo del libro:");
+
 String titulo;
-titulo = "Java desde cero";
-	
+
+titulo = sc.nextLine();
+System.out.println("Libro registrado:");
+
 System.out.println(titulo);
 	}
 
